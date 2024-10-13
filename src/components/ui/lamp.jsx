a2f8e225f290d085775b7ed13212d7edb9a96ac5
aayushai <aayushhp17@gmail.com>
2024-10-14 01:22:59 +0530

    Fix: some delays and durations

--- src/components/ui/lamp.jsx
+++ src/components/ui/lamp.jsx
@@ -9,8 +9,8 @@ export default function LampDemo() {
         initial={{ opacity: 0.5, y: 100 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{
-          delay: 0.3,
-          duration: 0.8,
+          delay: 0.6,
+          duration: 1,
           ease: "easeInOut",
         }}
         className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl">
@@ -36,8 +36,8 @@ export const LampContainer = ({
           initial={{ opacity: 0.5, width: "15rem" }}
           whileInView={{ opacity: 1, width: "30rem" }}
           transition={{
-            delay: 0.3,
-            duration: 0.8,
+          delay: 0.6,
+          duration: 1,
             ease: "easeInOut",
           }}
           style={{
@@ -53,8 +53,8 @@ export const LampContainer = ({
           initial={{ opacity: 0.5, width: "15rem" }}
           whileInView={{ opacity: 1, width: "30rem" }}
           transition={{
-            delay: 0.3,
-            duration: 0.8,
+          delay: 0.6,
+          duration: 1,
             ease: "easeInOut",
           }}
           style={{
@@ -76,8 +76,8 @@ export const LampContainer = ({
           initial={{ width: "8rem" }}
           whileInView={{ width: "16rem" }}
           transition={{
-            delay: 0.3,
-            duration: 0.8,
+          delay: 0.6,
+          duration: 1,
             ease: "easeInOut",
           }}
           className="absolute inset-auto z-30 h-36 w-64 -translate-y-[6rem] rounded-full bg-cyan-400 blur-2xl"></motion.div>
@@ -85,8 +85,8 @@ export const LampContainer = ({
           initial={{ width: "15rem" }}
           whileInView={{ width: "30rem" }}
           transition={{
-            delay: 0.3,
-            duration: 0.8,
+          delay: 0.6,
+          duration: 1,
             ease: "easeInOut",
           }}
           className="absolute inset-auto z-50 h-0.5 w-[30rem] -translate-y-[7rem] bg-cyan-400 "></motion.div>
