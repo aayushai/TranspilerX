2e74be0345061bcd02ca161eb90b4f1d54f7b62c
aayushai <aayushhp17@gmail.com>
2024-10-19 20:34:48 +0530

    Fix: empty-copy-message

--- src/CodeConverter.jsx
+++ src/CodeConverter.jsx
@@ -194,7 +194,7 @@ const CodeConverter = () => {
     }
   };
 
-  // const handleCopy = (code, fieldType) => {
+  const handleCopy = (code, fieldType) => {
       if (!inputCode.trim()) {
       toast.error(`${fieldType} field is empty!`, {
         position: 'top-right',
